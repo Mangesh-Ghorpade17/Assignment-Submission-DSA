3aa32d9493f1fcde107374b44dbe383f824f3b87
Mangesh-Ghorpade <74098684+Mangesh-Ghorpade17@users.noreply.github.com>
2021-02-06 18:44:20 +0530

    Day 1 Assignment Submission
    Output of Assignments :
    
    Q1.
    Enter First Number:5
    Enter second Number:20
    Addition of 2 Numbers: 25.0
    Substraction of 2 Numbers: -15.0
    Multiplication of 2 Numbers: 100.0
    Division of 2 Numbers: 0.25
    
    Q2.
    Enter Number :6
    Enter power :3
    Power of X ^ Y: 216

---
+++ Day 1/Assignment 1 Q1.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter First Number :"))
+n2=int(input("Enter second Number :"))
+print("Addition of 2 Numbers:",(n1+n2))
+print("Substraction of 2 Numbers:",(n1-n2))
+print("Multiplication of 2 Numbers:",(n1*n2))
+print("Division of 2 Numbers:",(n1/n2))
---
+++ Day 1/Assignment 1 Q2.py
@@ -0,0 +1,5 @@
+X=int(input("Enter Number :"))
+Y=int(input("Enter power :"))
+result=pow(X,Y)
+print("Power of X ^ Y:",result)
+
